89940705ccf7131d089ea33f77f6168681525c1e
woDong <wodong526@dingtalk.com>
2022-12-09 14:43:18 +0800

    验证云端

--- new_tools/data/Bridge_To_Maya/DHI/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import sys
-import os
-
-sys.path.append(os.path.join(os.path.dirname(__file__), 'pose_wrangler'))
-# Empty Init file
-from . import *
